79f3686f37f8ca2fe17c879dc9f95d4ad9a0ebba
mikerlaz903 <miker.laz903@gmail.com>
2021-05-05 12:56:39 +0300

    Class initialization and result class

---
+++ UniversalDatabase/DbResult.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UniversalDatabase
+{
+    public class DbResult
+    {
+        private readonly List<string> _fieldNames = new List<string>();
+        public object Value { get; private set; }
+        public DataTable ExecutedSqlInfo { get; }
+
+        public object this[int col]
+        {
+            get
+            {
+                return Value switch
+                {
+                    List<object> list => list[col],
+                    List<List<object>> matrix => (from row in matrix select row[col]).ToList(),
+                    _ => throw new InvalidOperationException("Indexer can be applied only after IEnumerable result")
+                };
+            }
+        }
+
+        public object this[string colName]
+        {
+            get
+            {
+                return Value switch
+                {
+                    List<object> list => list[_fieldNames.FindIndex(row => row.Equals(colName))],
+                    List<List<object>> matrix => (from row in matrix select row[_fieldNames.FindIndex(match => match.Equals(colName))]).ToList(),
+                    _ => throw new InvalidOperationException("Indexer can be applied only after IEnumerable result")
+                };
+            }
+        }
+
+        public DbResult(object result, DataTable info)
+        {
+            _fieldNames = new List<string>();
+
+            Value = result;
+            ExecutedSqlInfo = info;
+
+            DefineFieldNames();
+        }
+
+        private void DefineFieldNames()
+        {
+            if (ExecutedSqlInfo == null) return;
+            foreach (var row in ExecutedSqlInfo.AsEnumerable())
+            {
+                _fieldNames.Add(row.Field<string>("ColumnName"));
+            }
+        }
+
+        public void Clear()
+        {
+            Value = null;
+            ExecutedSqlInfo.Clear();
+        }
+    }
+}
--- UniversalDatabase/UDatabase.cs
+++ UniversalDatabase/UDatabase.cs
@@ -1,8 +1,41 @@
 ﻿using System;
+using System.Data;
+using System.Data.Common;
 
 namespace UniversalDatabase
 {
     public class UDatabase
     {
+        private static UDatabase _instance;
+        private static readonly object _syncRoot = new object();
+
+        private static DbConnection _connection;
+        private static DbTransaction _transaction;
+
+        private UDatabase(DbConnection connetion)
+        {
+            _connection = connetion;
+            _connection.Open();
+            _transaction = _connection.BeginTransaction(IsolationLevel.ReadCommitted);
+        }
+
+        public static UDatabase GetInstance(DbConnection connetion = null)
+        {
+            lock (_syncRoot)
+            {
+                if (_instance == null && connetion != null)
+                    _instance = new UDatabase(connetion);
+            }
+            return _instance;
+        }
+
+        private DbCommand GetCommand(string inputCommand)
+        {
+            var command = _connection.CreateCommand();
+            command.Transaction = _transaction;
+            command.CommandText = inputCommand;
+
+            return command;
+        }
     }
 }
